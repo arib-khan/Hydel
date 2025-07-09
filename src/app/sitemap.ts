ce24186294e47fed3f88b4d8f4f122ea8b714e8b
Arib khan <158550918+arib-khan@users.noreply.github.com>
2025-07-09 18:40:46 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,25 +5,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
     {
-      url: 'https://www.hydel.in',
+      url: 'https://www.hydel.co.in',
       lastModified: lastMod,
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://www.hydel.in/aboutus',
+      url: 'https://www.hydel.co.in/aboutus',
       lastModified: lastMod,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://www.hydel.in/products',
+      url: 'https://www.hydel.co.in/products',
       lastModified: lastMod,
       changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
-      url: 'https://www.hydel.in/contactus',
+      url: 'https://www.hydel.co.in/contactus',
       lastModified: lastMod,
       changeFrequency: 'monthly',
       priority: 0.7,
