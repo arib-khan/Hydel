df137db5343732b5b12f00f1989eaf8f57d96535
Arib khan <158550918+arib-khan@users.noreply.github.com>
2025-08-05 19:02:56 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,30 +3,33 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   const lastMod = new Date().toISOString();
 
-  return [
+  const staticRoutes: MetadataRoute.Sitemap = [
     {
-      url: 'https://www.hydel.co.in',
+      url: 'https://www.hydel.in',
       lastModified: lastMod,
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://www.hydel.co.in/aboutus',
+      url: 'https://www.hydel.in/aboutus',
       lastModified: lastMod,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://www.hydel.co.in/products',
+      url: 'https://www.hydel.in/products',
       lastModified: lastMod,
       changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
-      url: 'https://www.hydel.co.in/contactus',
+      url: 'https://www.hydel.in/contactus',
       lastModified: lastMod,
       changeFrequency: 'monthly',
       priority: 0.7,
     },
   ];
+
+
+  return [...staticRoutes];
 }
